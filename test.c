660f87156930a6182a24944dbc0fa2d08824d6df
yashp1602 <127816836+yashp1602@users.noreply.github.com>
2023-03-14 15:34:09 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,223 @@
+#include<stdio.h>
+main()
+{
+//	int a=67, b=3,sum;
+//	sum=a+b;
+//	printf("%i",sum);
+//	
+//	int c=25, d=5,x;
+//	x=c*d;
+//	printf("\n%i",x);
+//	
+//	int e=25, f=5,y;
+//	y=e/f;
+//	printf("\n%i",y);
+//	
+//	int j;
+//	printf("enter the value of j ");
+//	scanf("%i",&j);
+//	printf("the value of a is %i",j);
+//	
+//	int r,t;
+//	printf("enter the value of r ");
+//	scanf("%i",&r);
+//	printf("enter the value of t ");
+//	scanf("%i",&t);
+//	printf("the value of a is %i",r+t);
+//	
+//	int u,g;
+//	printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("enter the value of g ");
+//	scanf("%i",&g);
+//	printf("the value of a is %i",u-g);
+	
+//	int v,j;
+//	printf("enter the value of v ");
+//	scanf("%i",&v);
+//	printf("enter the value of j ");
+//	scanf("%i",&j);
+//	printf("the value of a is %i",v*j);
+	
+	
+//	int u,g;
+//	printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("enter the value of g ");
+//	scanf("%i",&g);
+//	printf("the value of a is %i",u/g);
+	
+	
+//	int u,g;
+//	printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("enter the value of g ");
+//	scanf("%i",&g);
+//	printf("the value of a is %i",u%g);
+
+
+//int u,g,k;
+//k=u-g;
+//	printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("enter the value of g ");
+//	scanf("%i",&g);
+//	printf("enter the value of k ");
+//	scanf("%i",&k);
+//	printf("the value of a is %i",u-g,k);
+
+
+//    int u;
+//    printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("the value of a is %i",u*u);
+	
+//	int u;
+//    printf("enter the value of u ");
+//	scanf("%i",&u);
+//	printf("the value of a is %i",u*u*u);
+	
+	
+	
+	
+//Area of Circle------------
+//	
+//float radius,area;
+//printf("enter the value of ");
+//scanf("%f",&radius);
+//	
+//area=5.33 * radius * radius;
+//printf("this area of circle %f",area);
+
+
+
+
+
+//Area of Rectangle------------
+//
+//int length, width, area;
+//printf("\nenter length :");
+//scanf("%i", &length);
+//
+//printf("\nenter width :");
+//scanf("%i", &width);
+//
+//area = length * width;
+//
+//printf("area of rectangle is : %i", area);
+
+
+
+
+//Area of Triangle------------
+//
+//float base, height, area;
+//printf("\nenter base :");
+//scanf("%f", &base);
+//
+//printf("\nenter height :");
+//scanf("%f", &height);
+//
+//area = 0.5 * base * height;
+//
+//printf("area of triangle is : %f", area);
+
+
+
+
+
+//Calculate Simple intrest------------
+//
+//float amount, rate, time, intrest;
+//printf("\nenter the amount : ");
+//scanf("%f", &amount);
+//
+//printf("\nenter the rate : ");
+//scanf("%f", &rate);
+//
+//printf("\nenter the time : ");
+//scanf("%f", &time);
+//
+//intrest = (amount * rate * time) / 100;
+//
+//printf("simple intrest is : %f", intrest);
+
+	
+//year is a leap year or no------------	
+//
+//int y;
+//printf("enter any year : ");
+//scanf("%i", &y);
+//if(y%400 == 0 || y%4==0 && y%100!=0)
+//{
+//	printf("year is leapyear");
+//}else
+//{
+//	printf("year is not leapyear");
+//}
+
+
+
+//Increment------------
+//    int x;
+//    printf("enter value x : ");
+//	scanf("%i",&x);
+//	printf("%i",++x);
+	
+
+
+//Increment ------------	
+//	int x;
+//    printf("enter value x : ");
+//	scanf("%i",&x);
+//	printf("%i",--x);
+	
+	
+	
+//	
+//	int u;
+//	printf("enter value u :");
+//	scanf("%i",&u);
+//	u*=2;
+//	printf("%i",u);
+	
+	
+	
+//	int x;
+//	printf("enter value x :");
+//	scanf("%i",&x);
+//	
+//	printf("%i", x<3 && x<8);
+	
+	
+//	int x;
+//	printf("enter value x :");
+//	scanf("%i",&x);
+//	
+//	printf("%i", x<3 || x<8);
+	
+	
+	
+//	int x;
+//	printf("enter value x :");
+//	scanf("%i",&x);
+//	
+//	printf("%i", !(x<3 && x<8));
+	
+	
+//	char a;
+//	int a;
+	float a;
+	printf("%i", sizeof(a));
+	
+	
+	
+	
+
+
+
+	
+
+
+	
+}
